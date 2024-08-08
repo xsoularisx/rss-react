9c5c159d81659a32a20471ddda4385f2955331da
albert <xsoularisx@gmail.com>
2024-08-08 21:09:42 +0300

    fix: remove old folder

--- class-components/src/test/main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-describe('App', () => {
-  it('should display the "throw error" text', () => {
-    render(<App />);
-    expect(screen.getByText('throw error')).toBeInTheDocument();
-  });
-});
